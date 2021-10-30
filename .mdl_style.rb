b798fee14505b0109cced8ca9ad61ce36cf05331
Alice Carroll <git@alice-carroll.ml>
2021-10-30 23:54:44 +0300

    ignore some issues for now

--- .mdl_style.rb
+++ .mdl_style.rb
@@ -1,7 +1,13 @@
 all
+
 rule "line-length", :line_length => 100
 rule "header-style", :style => :atx
 rule "ul-style", :style => :dash
 rule "ol-prefix", :style => :ordered
 rule "hr-style", :style => "---"
 rule "code-block-style", :style => :fenced
+
+# TODO
+exclude_rule "no-inline-html"
+exclude_rule "first-line-h1"
+exclude_rule "no-blanks-blockquote"
