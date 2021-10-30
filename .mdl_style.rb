5687c202040b064eb16b1be082a3b2e1e11ebfbb
Alice Carroll <git@alice-carroll.ml>
2021-10-30 23:04:59 +0300

    add mdl config

---
+++ .mdl_style.rb
@@ -0,0 +1,7 @@
+all
+rule "line-length", :line_length => 100
+rule "header-style", :style => :atx
+rule "ul-style", :style => :dash
+rule "ol-prefix", :style => :ordered
+rule "hr-style", :style => "---"
+rule "code-block-style", :style => :fenced
