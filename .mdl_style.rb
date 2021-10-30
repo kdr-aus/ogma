4af974ba35885d4e4a505ca69413f70c78de49e8
Alice Carroll <git@alice-carroll.ml>
2021-10-31 00:54:23 +0300

    definitely exclude `no-blanks-blockquote` rule

--- .mdl_style.rb
+++ .mdl_style.rb
@@ -6,8 +6,8 @@ rule "ul-style", :style => :dash
 rule "ol-prefix", :style => :ordered
 rule "hr-style", :style => "---"
 rule "code-block-style", :style => :fenced
+exclude_rule "no-blanks-blockquote"
 
 # TODO
 exclude_rule "no-inline-html"
 exclude_rule "first-line-h1"
-exclude_rule "no-blanks-blockquote"
