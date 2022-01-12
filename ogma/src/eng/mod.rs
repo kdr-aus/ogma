0f552ca43bb9f6b0bbb68045f2a069f5c69859d1
Kurt Lawrence <kurtlawrence92@gmail.com>
2022-01-12 18:51:43 +1000

    test the literal annotations

--- ogma/src/eng/mod.rs
+++ ogma/src/eng/mod.rs
@@ -66,60 +66,6 @@ impl<'d, 'v> Block<'d, 'v> {
         Ok(arg)
     }
 
-    fn arg_recursive(&self, arg: ast::Argument, in_ty: Type, locals: &Locals) -> Result<Argument> {
-        use ast::Argument as A;
-        use eval::make_input_pound_expr;
-
-        let (hold, tag, out_ty) = match arg {
-            A::Ident(ident) => (Hold::Lit(Str::new(ident.str()).into()), ident, Type::Str),
-            A::Num(n, tag) => (Hold::Lit(n.into()), tag, Type::Num),
-            A::Pound('t', tag) => (Hold::Lit(true.into()), tag, Type::Bool),
-            A::Pound('f', tag) => (Hold::Lit(false.into()), tag, Type::Bool),
-            A::Pound('n', tag) => (Hold::Lit(Value::Nil), tag, Type::Nil),
-            A::Pound('i', tag) => (
-                Hold::Expr(make_input_pound_expr(in_ty.clone(), tag.clone())),
-                tag,
-                in_ty.clone(),
-            ),
-            A::Pound(ch, tag) => return Err(Error::unknown_spec_literal(ch, &tag)),
-            A::Var(var) => {
-                match locals
-                    .get(var.str())
-                    .ok_or_else(|| Error::var_not_found(&var))?
-                {
-                    Local::Param(arg, locals) => {
-                        // update result with the outside var (similar to Local::Var)
-                        return self
-                            .arg_recursive(arg.clone(), in_ty, locals)
-                            .map_err(|e| e.add_trace(&var))
-                            .map(|mut x| (x.tag = var, x).1);
-                    }
-                    Local::Var(v) => {
-                        let mut v = v.clone();
-                        // update the location of this var to give correct error reporting
-                        v.tag = var.clone();
-                        let ty = v.ty().clone();
-                        (Hold::Var(v), var, ty)
-                    }
-                }
-            }
-            A::Expr(expr) => {
-                let tag = expr.tag.clone();
-                let eval = Evaluator::construct(in_ty.clone(), expr, self.defs, locals.clone())
-                    .map_err(|e| e.add_trace(&self.blk_tag))?;
-                let out_ty = eval.ty().clone();
-                (Hold::Expr(eval), tag, out_ty)
-            }
-        };
-
-        Ok(Argument {
-            tag,
-            in_ty,
-            out_ty,
-            hold,
-        })
-    }
-
     fn finalise(&self) -> Result<()> {
         if let Some(flag) = self.flags.last() {
             Err(Error::unused_flag(flag))
@@ -139,3 +85,51 @@ pub struct Step {
 }
 
 type StepR = Result<(Value, Environment)>;
+
+// ###### FUNCTIONS ############################################################
+
+// fn walk_argument(arg: ast::Argument) {
+//     use ast::Argument as A;
+//
+//     match arg {
+//         A::Ident(ident) => (Hold::Lit(Str::new(ident.str()).into()), ident, Type::Str),
+//         A::Num(n, tag) => (Hold::Lit(n.into()), tag, Type::Num),
+//         A::Pound('t', tag) => (Hold::Lit(true.into()), tag, Type::Bool),
+//         A::Pound('f', tag) => (Hold::Lit(false.into()), tag, Type::Bool),
+//         A::Pound('n', tag) => (Hold::Lit(Value::Nil), tag, Type::Nil),
+//         A::Pound('i', tag) => (
+//             Hold::Expr(make_input_pound_expr(in_ty.clone(), tag.clone())),
+//             tag,
+//             in_ty.clone(),
+//         ),
+//         A::Pound(ch, tag) => return Err(Error::unknown_spec_literal(ch, &tag)),
+//         A::Var(var) => {
+//             match locals
+//                 .get(var.str())
+//                 .ok_or_else(|| Error::var_not_found(&var))?
+//             {
+//                 Local::Param(arg, locals) => {
+//                     // update result with the outside var (similar to Local::Var)
+//                     return self
+//                         .arg_recursive(arg.clone(), in_ty, locals)
+//                         .map_err(|e| e.add_trace(&var))
+//                         .map(|mut x| (x.tag = var, x).1);
+//                 }
+//                 Local::Var(v) => {
+//                     let mut v = v.clone();
+//                     // update the location of this var to give correct error reporting
+//                     v.tag = var.clone();
+//                     let ty = v.ty().clone();
+//                     (Hold::Var(v), var, ty)
+//                 }
+//             }
+//         }
+//         A::Expr(expr) => {
+//             let tag = expr.tag.clone();
+//             let eval = Evaluator::construct(in_ty.clone(), expr, self.defs, locals.clone())
+//                 .map_err(|e| e.add_trace(&self.blk_tag))?;
+//             let out_ty = eval.ty().clone();
+//             (Hold::Expr(eval), tag, out_ty)
+//         }
+//     }
+// }
--- ogma/src/eng/ty/annotate.rs
+++ ogma/src/eng/ty/annotate.rs
@@ -1,26 +1,55 @@
 //! Module handling type _annotations_, human readable type information.
 
 use crate::prelude::*;
+use std::fmt;
 
-pub fn annotate_expression(arg: &ast::Argument) -> String {
+fn annotate_argument(arg: &ast::Argument, in_ty: Type) -> Result<String> {
     use ast::Argument::*;
 
-    match arg {
-        Ident(ident) => {
-            format!("{}:Str", ident)
-        }
-        Num(_num, tag) => {
-            format!("{}:Num", tag)
-        }
+    // TODO the Pound matching code is repetitive with arg_recursive,
+    // Work out how to generalise this.
+    let (tag, out_ty) = match arg {
+        Ident(ident) => (ident, Type::Str),
+        Num(_num, tag) => (tag, Type::Num),
+        Pound('t', tag) => (tag, Type::Bool),
+        Pound('f', tag) => (tag, Type::Bool),
+        Pound('n', tag) => (tag, Type::Nil),
+        Pound('i', tag) => (tag, in_ty),
+        Pound(ch, tag) => return Err(Error::unknown_spec_literal(*ch, &tag)),
         x => todo!(),
+    };
+
+    Ok(format!("{}:{}", tag, AnnotationDisplay(out_ty)))
+}
+
+struct AnnotationDisplay(Type);
+
+impl fmt::Display for AnnotationDisplay {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use Type::*;
+
+        let x = match &self.0 {
+            Nil => "Nil",
+            Bool => "Bool",
+            Num => "Num",
+            Str => "Str",
+            Tab => "Table",
+            TabRow => "TableRow",
+            Def(d) => d.name().str(),
+        };
+
+        write!(f, "{}", x)
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use annotate_expression as ae;
     use ast::{Argument::*, *};
+
+    fn aa(arg: ast::Argument) -> String {
+        annotate_argument(&arg, Type::Tab).unwrap()
+    }
 
     fn tag(s: &str) -> Tag {
         Tag {
@@ -33,11 +62,19 @@ mod tests {
 
     #[test]
     fn annotate_ident() {
-        assert_eq!(ae(&Ident(tag("foo-bar"))), "foo-bar:Str");
+        assert_eq!(aa(Ident(tag("foo-bar"))), "foo-bar:Str");
     }
 
     #[test]
     fn annotate_number() {
-        assert_eq!(ae(&Num(1.into(), tag("1.0"))), "1.0:Num");
+        assert_eq!(aa(Num(1.into(), tag("1.0"))), "1.0:Num");
+    }
+
+    #[test]
+    fn annotate_literal() {
+        assert_eq!(aa(Pound('t', tag("#t"))), "#t:Bool");
+        assert_eq!(aa(Pound('f', tag("#f"))), "#f:Bool");
+        assert_eq!(aa(Pound('n', tag("#n"))), "#n:Nil");
+        assert_eq!(aa(Pound('i', tag("#i"))), "#i:Table");
     }
 }
