8d4b42c440a3bca8e55707aba5cb1c054af519b6
Kurt Lawrence <kurtlawrence92@gmail.com>
2022-03-22 10:46:05 +1000

    test cli

--- bin/app.rs
+++ bin/app.rs
@@ -1,6 +1,6 @@
 use clap::*;
 
-#[derive(Parser)]
+#[derive(Parser, PartialEq, Debug)]
 #[clap(name = "ogma")]
 #[clap(author = "Kurt Lawrence <kurtlawrence92@gmail.com>")]
 #[clap(about = "Scripting language for manipulating tabular data")]
@@ -15,4 +15,88 @@ pub struct App {
 
     /// Files to process. If none specified, a REPL instance is started.
     pub files: Vec<String>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::App;
+    use super::*;
+
+    #[test]
+    fn verify_app_01() {
+        App::command().debug_assert();
+    }
+
+    #[test]
+    fn verify_app_02() {
+        let a = App::try_parse_from(["ogma"]).unwrap();
+        assert_eq!(
+            a,
+            App {
+                quiet: false,
+                def: None,
+                files: vec![]
+            }
+        );
+
+        let a = App::try_parse_from("ogma -q".split(' ')).unwrap();
+        assert_eq!(
+            a,
+            App {
+                quiet: true,
+                def: None,
+                files: vec![]
+            }
+        );
+
+        let a = App::try_parse_from("ogma --def foo".split(' ')).unwrap();
+        assert_eq!(
+            a,
+            App {
+                quiet: false,
+                def: Some(vec!["foo".to_string()]),
+                files: vec![]
+            }
+        );
+
+        let a = App::try_parse_from("ogma --def foo --def bar".split(' ')).unwrap();
+        assert_eq!(
+            a,
+            App {
+                quiet: false,
+                def: Some(vec!["foo".to_string(), "bar".to_string()]),
+                files: vec![]
+            }
+        );
+
+        let a = App::try_parse_from("ogma foo".split(' ')).unwrap();
+        assert_eq!(
+            a,
+            App {
+                quiet: false,
+                def: None,
+                files: vec!["foo".to_string()]
+            }
+        );
+
+        let a = App::try_parse_from("ogma foo bar".split(' ')).unwrap();
+        assert_eq!(
+            a,
+            App {
+                quiet: false,
+                def: None,
+                files: vec!["foo".to_string(), "bar".to_string()]
+            }
+        );
+
+        let a = App::try_parse_from("ogma --def=foo --def bar foo bar".split(' ')).unwrap();
+        assert_eq!(
+            a,
+            App {
+                quiet: false,
+                def: Some(vec!["foo".to_string(), "bar".to_string()]),
+                files: vec!["foo".to_string(), "bar".to_string()]
+            }
+        );
+    }
 }
