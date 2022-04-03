18f69a757864350741afc25c9cfa121bb4d2ef9e
Kurt Lawrence <kurtlawrence92@gmail.com>
2022-04-03 12:09:31 +1000

    test 11 No Recursion

--- ogma/tests/doc-examples.rs
+++ ogma/tests/doc-examples.rs
@@ -203,3 +203,73 @@ let {get:Num price} $price {get:Num carat} $ct | \$price | / $ct }"#,
 
     assert_eq!(a, b);
 }
+
+// #### 11 (No) Recursion ######################################################
+
+#[test]
+fn _11_0_no_recursion_01() {
+    let defs = &mut Definitions::new();
+
+    process_def(
+        r#"def pwr (n x) { if { \$x | = 0 } 1 { range 1 $x | fold $n * $n } }"#,
+        defs,
+    );
+
+    let x = process("pwr 2 3", defs);
+    assert_eq!(x, Ok(Value::Num(8u8.into())));
+    let x = process("pwr 2 2", defs);
+    assert_eq!(x, Ok(Value::Num(4u8.into())));
+    let x = process("pwr 2 1", defs);
+    assert_eq!(x, Ok(Value::Num(2u8.into())));
+    let x = process("pwr 2 0", defs);
+    assert_eq!(x, Ok(Value::Num(1u8.into())));
+}
+
+#[test]
+fn _11_0_no_recursion_02() {
+    let defs = &mut Definitions::new();
+
+    process_def(
+        r#"def fact Num () { if {<= 1} 1 { + 1 | range 1 | fold 1 * $row.i } }"#,
+        defs,
+    );
+
+    let x = process("range 1 11 | append --'i!' { get i | fact }", defs);
+
+    let fact = |n: u8| (1..=n).map(|x| x as f64).fold(1f64, std::ops::Mul::mul);
+
+    let exp = vec![
+        vec![s("i"), s("i!")],
+        vec![n(1u8), n(fact(1))],
+        vec![n(2u8), n(fact(2))],
+        vec![n(3u8), n(fact(3))],
+        vec![n(4u8), n(fact(4))],
+        vec![n(5u8), n(fact(5))],
+        vec![n(6u8), n(fact(6))],
+        vec![n(7u8), n(fact(7))],
+        vec![n(8u8), n(fact(8))],
+        vec![n(9u8), n(fact(9))],
+        vec![n(10u8), n(fact(10))],
+    ];
+
+    check_is_table(x, exp);
+}
+
+#[test]
+fn _11_0_no_recursion_03() {
+    let defs = &mut Definitions::new();
+
+    process_def(r#"def gcd (a b) { range 0 {\$a|abs|min {\$b|abs}} |
+    fold-while { if {\$a|< $b} {Tuple $b $a} {Tuple $a $b} }
+        { get t1 | != 0 }
+        { let $acc | get t0 | mod $acc.t1 | Tuple $acc.t1 #i }
+    | get t0
+}"#, defs);
+
+let x = process("gcd 10 4", defs);
+assert_eq!(x, Ok(Value::Num(2u8.into())));
+let x = process("gcd 100 80", defs);
+assert_eq!(x, Ok(Value::Num(20u8.into())));
+let x = process("gcd 255 25", defs);
+assert_eq!(x, Ok(Value::Num(5u8.into())));
+}
